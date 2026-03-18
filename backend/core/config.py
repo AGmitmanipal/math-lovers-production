2fa1d4ae8e75c4d2d7f8bced894395871afebde0
AGmitmanipal <ghodeanay@gmail.com>
2026-03-18 12:26:17 +0530

    Initial commit

---
+++ backend/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+
+    DATABASE_URL: str
+
+    COOKIE_SECURE: bool = False 
+    COOKIE_HTTPONLY: bool = True
+    COOKIE_SAMESITE: str = "lax"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
